e1853ea4bb035a4c69294d22d34dcb9f71ec5aba
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2025-07-01 20:56:23 -0300

    commit 01/07/2025 at 20:56

--- src/main/java/med/net/api/controller/MedicoController.java
+++ src/main/java/med/net/api/controller/MedicoController.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/medico")
+@RequestMapping("/medicos")
 public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
