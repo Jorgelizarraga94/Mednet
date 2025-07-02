414e2c1f0e128e738e2a32c2d962ec775906e7ad
Jorgelizarraga94 <jorgelizarraga1994@gmail.com>
2025-07-02 20:00:48 -0300

    commit 02/07/2025 at 20:00

--- src/main/java/med/net/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/net/api/infra/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package med.net.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,15 +13,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private SecurityFilter securityFilter;
     //securityFilterChain cambia las configuraciones base de Spring Security por las que nosotros deseamos, cambiar de statefull a stateless
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm-> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    request.anyRequest().authenticated();
+                })
+                //Le damos la instruccion de aplicar primero nuestro filter, y luego el filter de spring generado en  la linea 28 automaticamente
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
--- src/main/java/med/net/api/infra/security/SecurityFilter.java
+++ src/main/java/med/net/api/infra/security/SecurityFilter.java
@@ -4,7 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.net.api.domain.Usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,20 +17,29 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
     private TokenService tokenService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJwt = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJwt);
-        System.out.println(subject);
-        filterChain.doFilter(request,response);
+        if(tokenJwt != null){
+            var subject = tokenService.getSubject(tokenJwt);
+            var usuario = usuarioRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+            filterChain.doFilter(request,response);
+
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("authorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("token jwt no enviado en el encabezado de authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer " , "");
         }
-        return authorizationHeader.replace("Bearer " , "");
+        return null;
     }
 }
